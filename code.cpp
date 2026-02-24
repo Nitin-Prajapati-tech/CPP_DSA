09550850e15c0a26bb8a45063f4f71e4ecdd26cb
Nitin Prajapati <nkumar25122@gmail.com>
2026-02-24 21:31:14 +0530

    Started some basic codes

---
+++ code.cpp
@@ -0,0 +1,17 @@
+//Boilerplate code
+/*#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world!"<<endl;
+    return 0;
+}*/
+//print pattern
+/*#include<iostream>
+using namespace std;
+int main(){
+    cout<<"****\n***\n**\n*\n"<<endl;
+    return 0;
+}*/
+//
+
+
