b41f8fd58107f7229d0011a96138cb52a68d75d0
Nitin Prajapati <nkumar25122@gmail.com>
2026-02-24 21:40:03 +0530

    some file are added

---
+++ Basic codes/boilerplate.cpp
@@ -0,0 +1,8 @@
+// Boilerplate code
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ Basic codes/pattern1.cpp
@@ -0,0 +1,7 @@
+//print pattern
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"****\n***\n**\n*\n"<<endl;
+    return 0;
+}
