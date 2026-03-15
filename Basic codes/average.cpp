11b93b3ad86557441a0a16c938a32a2c46c2b85f
Nitin Prajapati <nkumar25122@gmail.com>
2026-03-15 22:32:39 +0530

    Update average.cpp

--- Basic codes/average.cpp
+++ Basic codes/average.cpp
@@ -5,14 +5,13 @@ int main(){
     int maths;
     int physics;
     int chemistry;
-    cout<<"Enetr the marks of maths :";
-    cin>>maths;
-
-    cout<<"Enetr the marks of physics :";
+    cout<<"Enter the marks of math :";
+    cin>>math;
+    cout<<"Enter the marks of physics :";
     cin>>physics;
-    cout<<"Enetr the marks of chemistry :";
+    cout<<"Enter the marks of chemistry :";
     cin>>chemistry;
-    int avg = (maths+physics+chemistry)/3;//if we are divide integer number ot integer number  then the return output is also integer number
+    int avg = (math+physics+chemistry)/3;//if we are divide integer number ot integer number  then the return output is also integer number
     cout<<"the average marlks is = "<<avg<<endl;
     return 0;
 
