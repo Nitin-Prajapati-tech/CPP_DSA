32c77b42562d08fc089526ccbec181aca36d106f
Nitin Kumar <Nkumar25122@gmail.com>
2026-04-08 23:48:43 +0530

    update file

--- Araays/file1.cpp
+++ Araays/file1.cpp
@@ -1,20 +1,37 @@
-#include<istream>
+#include<iostream>
 #include<climits> // INT_MAX is the part of climits header file;
 using namespace std;
 int main(){
-    //print smallest array;
+    /*//print smallest array;
     int nums[] = {12, 23, -34, 35, 24};
     int size = 5;
     
     
     int smallest = INT_MAX; //+infinate
-    
+
+
     for(int i = 0; i<size; i++){
+        //min function;
+        //smallest = min(num[i],smallest); its use then remove if blockes;
+
         if(nums[i]<smallest){
             smallest = nums[i];
         }
     }
-    cout << "smallest = "<<smallest<<endl;
+    cout << "smallest = "<<smallest<<endl;*/
+
+
+    // print largest array;
+    int nums[] = {23,34,56,-35,22};
+    int size = 5;
+    int largest = INT_MIN;
+    for(int i=0; i<size; i++){
+        if(largest<nums[i]){
+            largest = nums[i];
+        }
+    }
+    cout << "largest = "<<largest <<endl;
+
     return 0;
 
 }
