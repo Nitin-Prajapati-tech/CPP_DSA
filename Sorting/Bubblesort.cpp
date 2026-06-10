32148f3b2657f5fe5415478c5227be4371968f61
Nitin Kumar <Nkumar25122@gmail.com>
2026-06-11 00:23:25 +0530

    update Bubblesort

--- Sorting/Bubblesort.cpp
+++ Sorting/Bubblesort.cpp
@@ -3,22 +3,27 @@ using namespace std;
 void print(int arr[] , int n){
 
     for(int i = 0; i<n; i++){
+    bool isSwap = false; // there is no swap element;
         cout<< arr[i] << " "<<endl; 
     }
 }
 
 void bubbleSort(int arr[], int n){
-    for(int i = 0; i<n-1; i++){
-        for(int j = 0; j<n-i-1; j++){
-            if(arr[j] >arr[j+1]){
+    for(int i = 0; i<n-1; i++){ // outer loop;
+        for(int j = 0; j<n-i-1; j++){ //inner loop;
+            if(arr[j] > arr[j+1]){ //use ">" to sort decending order;
                 swap(arr[j], arr[j+1]);
+              
+
             }
         }
+        
     }
     print(arr, n);
 }
 int main(){
     int arr[5] = {5,4,1,3,2};
+    //int arr[10] = {1,2,3,4,5,6,7,8,9,10};
     int n = sizeof(arr) / sizeof(int);
     bubbleSort(arr, n);
     return 0;
