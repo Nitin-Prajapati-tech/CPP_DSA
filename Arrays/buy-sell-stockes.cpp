8d6494c0940e276aeeb08a2030bc8698e49a9b2d
Nitin Kumar <Nkumar25122@gmail.com>
2026-06-10 23:53:07 +0530

    buy-sell-stockes

--- Arrays/buy-sell-stockes.cpp
+++ Arrays/buy-sell-stockes.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 void  maxProfit ( int *prices , int n){
     int bestBuy[100000];
-    int bestBuy[0] = INT_MAX;
+    bestBuy[0] = INT_MAX;
     for(int i = 1; i<n; i++){
         bestBuy[i] = min(bestBuy[i-1], prices[i-1]);
     }
