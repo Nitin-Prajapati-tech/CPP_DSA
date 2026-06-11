71975492d199e9619bf4145586db9ad8c29ed013
Nitin Kumar <Nkumar25122@gmail.com>
2026-06-12 00:55:15 +0530

    diagonalMatrix

--- Arrays/Trapping-Rainwater.cpp
+++ Arrays/Trapping-Rainwater.cpp
@@ -4,21 +4,30 @@ void trap(int *height , int n){
     int leftMax[20000], rightMax[20000];
     
     leftMax[0] = height[0];
-    cout<<leftMax[0] <<",";
+    
     rightMax[n-1] = height[n-1];
-    cout<<rightMax[n-1]<<",";
+   
     for(int i = 1; i<n; i++){
         leftMax[i] = max(leftMax[i-1], height[i-1]);
-        cout<<leftMax[i]<<",";
+        
     }
     for(int i = n-2; i>=0; i--){
-        leftMax[i] = max(leftMax[i+1], height[i+1]);
-        cout<<leftMax[i]<<",";
+        rightMax[i] = max(rightMax[i+1], height[i+1]);
+        
     }
-    cout<<endl;
+    int waterTrapped = 0;
+    for(int i = 0; i<n; i++){
+        int currWater = min(leftMax[i], rightMax[i]) - height[i]; 
+        if(currWater > 0){
+            waterTrapped += currWater;
+        }
+
+    }
+    
+    cout<<"water trapped = "<<waterTrapped<<endl;
 }
 int main(){
-    int trap[7] = {4,2,0,6,3,2,5};
+    int height[7] = {4,2,0,6,3,2,5};
     int n = sizeof(height)/sizeof(int);
     trap(height, n);
     return 0;
