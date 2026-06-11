399def55b9c1daba091e8c17a5d0808a02a96c4b
Nitin Kumar <Nkumar25122@gmail.com>
2026-06-11 11:15:53 +0530

    update SelectionSort.cpp

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[] , int n ){
+    //o(n^2);
+    for(int i = 0; i<n-1; i++){
+        int minIdx = i;
+        //loop for find min
+        for(int j = i+1; j<n; j++){
+            if(arr[j] < arr[minIdx]){
+                minIdx = j;
+            }
+
+        }
+        swap(arr[i] , arr[minIdx]);
+    }
+   
+}
+int mian(){
+    int arr[5] = {4,5,7,2,6};
+    int n = sizeof(arr) / sizeof(int);
+    selectionSort(arr, n);
+    return 0;
+}
